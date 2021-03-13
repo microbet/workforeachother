8e5df764930a53ef3ecd42385826bb03366e886a
jhirsch <jay.softwaredev@gmail.com>
2021-03-13 00:03:23 -0800

    added navigation and moved things around

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :orders
   devise_for :users
   root 'welcome#index'
+  get 'welcome/about'
+  get 'welcome/contact'
+  get 'welcome/login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
