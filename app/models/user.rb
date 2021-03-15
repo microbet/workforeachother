3f1eb94c557792b6d08e41562020b0a1b35f19ca
jhirsch <jay.softwaredev@gmail.com>
2021-03-14 18:45:09 -0700

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
-  @@balance
   after_initialize :set_default_role, :if => :new_record?
-  after_initialize :set_balance, :if => :new_record?
+  #after_initialize :set_balance, :if => :new_record?
   after_create :make_deposit
 
 
@@ -10,9 +9,11 @@ class User < ActiveRecord::Base
     self.role ||= :user
   end
 
-  def set_balance
-    self.balance = -1
-  end
+  #def set_balance
+  #  puts "self is self"
+  #  puts self.inspect
+  #  self.balance = -1
+  #end
 
   def make_deposit
     deposit_params = { "amount"=>-1, "user_id"=>self.id }
